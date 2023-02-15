a0f5bf6c2edc374abde55697b36d9781a6edb1a1
최경선 / 서비스개발담당 WEB개발팀 <156506@ssg.com>
2023-02-15 15:54:56 +0900

    feat: 어셋, 아이콘 초기화

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dicee',
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        body: DicePage(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
